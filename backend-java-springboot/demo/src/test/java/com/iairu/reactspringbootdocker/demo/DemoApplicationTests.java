8b2aca1c189fcde0210357b0d823155a4a76b5a4
Ondrej Špánik <spanik11@gmail.com>
2025-10-21 13:46:43 +0200

    init backend springboot

---
+++ backend-java-springboot/demo/src/test/java/com/iairu/reactspringbootdocker/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iairu.reactspringbootdocker.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
